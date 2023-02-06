d062cd66abe4d8636f85fb90cb458020a5a1eed4
christopher coyne <christcoyne@gmail.com>
2023-02-05 22:56:02 -0500

    google strategy added for passport

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import session from 'express-session';
 import passport from 'passport';
+import passportGoogle from 'passport-google-oauth20';
+
+const GoogleStrategy = passportGoogle.Strategy;
 
 dotenv.config();
 
@@ -21,6 +24,41 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  return done(null, user);
+});
+
+passport.deserializeUser((user: any, done) => {
+  return done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      callbackURL: '/auth/google/callback',
+    },
+    function (accessToken: any, refreshToken: any, profile: any, cb) {
+      // Successful Auth
+      // insert into db
+      console.log('profile ', profile);
+      cb(null, profile);
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('http://localhost:3000');
+  }
+);
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
